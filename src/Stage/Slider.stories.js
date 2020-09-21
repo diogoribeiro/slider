555a63ba11b0dec5ddc9a48c82779413d85b9908
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2020-09-21 16:24:38 -0300

    Add Stage component

---
+++ src/Stage/Slider.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Stage from '@/Stage';
+
+export default {
+  title: 'Stage',
+  component: Stage,
+};
+
+export const StageExample = () => <Stage><img src="https://picsum.photos/seed/single/1700/300" alt="Stage example" /></Stage>;
---
+++ src/Stage/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const NEXT_ITEM = 1;
+const PREVIOUS_ITEM = -1;
+
+const Stage = ({ children, onTouch }) => {
+  const [initialPosition, setInitialPosition] = useState(0);
+
+  const initPosition = (e) => {
+    setInitialPosition(e.clientX);
+  };
+
+  const stopPosition = (e) => {
+    const finalPosition = e.clientX;
+    const factor = finalPosition < initialPosition ? PREVIOUS_ITEM : NEXT_ITEM;
+    onTouch(factor);
+  };
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+    <div className="stage" role="list" onTouchStart={initPosition} onTouchEnd={stopPosition} onMouseDown={initPosition} onMouseUp={stopPosition}>
+      {children}
+    </div>
+  );
+};
+
+Stage.propTypes = {
+  onTouch: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+Stage.defaultProps = {
+  children: [],
+};
+
+export default Stage;
